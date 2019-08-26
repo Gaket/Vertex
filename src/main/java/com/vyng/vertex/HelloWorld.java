5a0ce7555a072d38d1be9d50b8292e42e600aea6
Artur Badretdinov <artursletter@gmail.com>
2019-08-27 01:23:52 +0300

    Add default port

--- src/main/java/com/vyng/vertex/HelloWorld.java
+++ src/main/java/com/vyng/vertex/HelloWorld.java
@@ -5,17 +5,25 @@ import io.vertx.core.Vertx;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        String host = "0.0.0.0";
-        int port = Integer.parseInt(System.getenv("PORT"));
+        int port;
+        try {
+            port = Integer.parseInt(System.getenv("PORT"));
+        } catch (NumberFormatException e) {
+            // Port used if Vert.x is started locally
+            port = 5000;
+        }
+
+        int finalPort = port;
+
         // Create an HTTP server which simply returns "Hello World!" to each request.
         Vertx.vertx()
                 .createHttpServer()
                 .requestHandler(req -> req.response().end("Hello World!"))
                 .listen(port, handler -> {
                     if (handler.succeeded()) {
-                        System.out.println("http://localhost:" + port + "/");
+                        System.out.println("http://localhost:" + finalPort + "/");
                     } else {
-                        System.err.println("Failed to listen on port " + port);
+                        System.err.println("Failed to listen on port " + finalPort);
                     }
                 });
     }
